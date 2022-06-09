94c8f31e049a9609ae24029d4657fcd8495303db
RicardoGmPr <70128085+RicardoGmPr@users.noreply.github.com>
2022-06-09 11:40:27 -0700

    V1

---
+++ Examen_U6/Examen_U6/Productos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_U6
+{
+    public class Productos
+    {
+        public string nombre, descripcion;
+        public float precio;
+        public int cantidad;
+
+        public Productos(string nombre, string descripcion, float precio, int cantidad)
+        {
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.precio = precio;
+            this.cantidad = cantidad;
+        }
+    }
+}
---
+++ Examen_U6/Examen_U6/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Examen_U6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("Productos.txt", true);
+            
+            int num, opcion;
+            char resp;
+            String str;
+        do 
+        {
+            Console.WriteLine("**********Sistema De Inventarios Amazon**********");
+            Console.WriteLine("1.- Registrar producto.");
+            Console.WriteLine("2.- Lectura de un Archivo.");
+            Console.WriteLine("3.- Salida del Programa");
+            Console.Write("\nQue opción deseas: ");
+            opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                        Console.Clear();
+                        Console.WriteLine("**********Registro de productos**********");
+                        Console.WriteLine("¿Cual es el nombre del producto?");
+                        string newNombre = Console.ReadLine();
+                        Console.WriteLine("¿Cual es la descripción del producto?");
+                        string newDescripcion = Console.ReadLine();
+                        Console.WriteLine("¿Cual es su precio?");
+                        float newPrecio = float.Parse(Console.ReadLine());
+                        Console.WriteLine("¿Cual es la cantidad en Stock?");
+                        int newStock = int.Parse(Console.ReadLine());
+
+                        Productos productos = new Productos(newNombre, newDescripcion, newPrecio, newStock);
+                        Console.Clear();
+                        sw.WriteLine("Nombre: " + productos.nombre + "  |  Descripción: " + productos.descripcion + "  |  Precio: " + productos.precio + "  |  Cantidad en Stock: " + productos.cantidad); //Escribe en el archivo
+                        Console.WriteLine("Registro completado con exito!");
+                        break;
+                   case 2:
+                        Console.WriteLine(".....");
+
+                        break;
+                case 3:
+                    Console.Write("Presione Enter para salir");
+                    Console.ReadKey();
+                    break;
+                default:
+                        Console.Clear();
+                    Console.Write("Opción desconocida, presione Enter para volver");
+                    Console.ReadKey();
+                    break;
+            }
+        } while (opcion != 3);
+
+            sw.Close(); //Cierra el archivo
+        }
+    }
+}
